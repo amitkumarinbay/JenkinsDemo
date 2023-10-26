3dc09f397d321acafbb13de9aa36adfdd036cd61
admin <admin@DESKTOP-5MAMO98>
2023-10-26 22:15:54 +0530

    New project

---
+++ src/test/java/runner/RunCuke.java
@@ -0,0 +1,13 @@
+
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/Features/PracticeForm.feature", glue = "steps", plugin = {
+		"json:target/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, tags = "@Urgent")
+public class RunCuke extends AbstractTestNGCucumberTests {
+
+	// @DataTable and not @Smoke and not @Prod
+}
